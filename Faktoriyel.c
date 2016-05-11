57b60522262f8cc16f6aceb4b7b23e8e5b464033
Servet Şatıroğlu <satirogluservet@gmail.com>
2016-05-11 19:19:49 +0300

    Add files via upload

---
+++ Faktoriyel.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ 
+long faktoriyel(int);
+ 
+int main()
+{
+  int sayi;
+ 
+  printf("Bir Sayi Giriniz:\n");
+  scanf("%d", &sayi); 
+ 
+  if (sayi < 0)
+    printf("Negatif Bir Sayi Girdiniz!\n");
+  else
+  {
+    printf("%d! = %ld\n", sayi,faktoriyel(sayi));
+  }
+ 
+  return 0;
+}
+ 
+long faktoriyel(int sayi1)
+{
+  if (sayi1 == 0)
+    return 1;
+  else
+    return(sayi1 * faktoriyel(sayi1-1));
+}
